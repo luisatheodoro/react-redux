81c0ec30b47dd98b6e12d3fd93405c01218d360d
Luisa P <luisa.paoliello@avadolearning.com>
2020-10-06 23:00:36 +0100

    Refactoring test and using shallow function for app component

--- testing/src/components/__test__/App.test.js
+++ testing/src/components/__test__/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+import App from 'components/App';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App/>);
+});
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-    // expect(div.innerHTML).toContain('Comment Box');
-
-    ReactDOM.unmountComponentAtNode(div);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 });
+
+it('shows a comment list', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1);
+})
