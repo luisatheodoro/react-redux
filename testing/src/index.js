bdf311c3c3b1497c5768826f7c149d9dc86cc05e
Luisa P <luisa.paoliello@avadolearning.com>
2020-10-11 15:18:38 +0100

    Adding a Root helper to connect component and test components to the Provider wwithout having to import too many libraries everytime

--- testing/src/index.js
+++ testing/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from 'reducers';
+import Root from 'Root';
 
 ReactDOM.render(
-        <Provider store={createStore(reducers, {})}>
+        <Root>
             <App/>
-        </Provider>,
+        </Root>,
     document.getElementById('root')
 );
 
