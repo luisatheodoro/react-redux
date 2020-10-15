8d707d662b34150e8d9597f2586b84fb7089cbfa
Luisa P <luisa.paoliello@avadolearning.com>
2020-10-15 23:49:12 +0100

    Initializing, connecting and creating controllers with mongodb

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,10 +1,16 @@
 //Main starting point of the application
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://luisa:'+process.env.MYAPIKEY+'@cluster0.jcmbz.mongodb.net/auth?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 
 //App Setup
 app.use(morgan('combined'));
