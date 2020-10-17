47ec97933a11d1d2932e72366b3874d77d85664a
Luisa P <luisa.paoliello@avadolearning.com>
2020-10-17 22:01:33 +0100

    Fixing cors error and allowing our frontend port to work

--- auth/server/index.js
+++ auth/server/index.js
@@ -7,13 +7,19 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://luisa:'+process.env.MYAPIKEY+'@cluster0.jcmbz.mongodb.net/auth?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 //App Setup
 app.use(morgan('combined'));
+app.use(cors(corsOptions));
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
